06b1174290d9620cafb832afb324bcf38454dafd
leozw <lzwdeveloper@gmail.com>
2024-02-21 13:48:59 -0300

    feat: module lambda

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -1,5 +1,17 @@
 variable "lambdas" {
   description = "Map of Lambda function configurations"
-  type        = map(any)
-  default     = {}
+  type = map(object({
+    function_name       = string
+    runtime             = string
+    handler             = string
+    architectures       = list(string)
+    tracing_config      = string
+    memory_size         = number
+    timeout             = number
+    s3_bucket           = string
+    s3_key              = string
+    additional_policies = list(string)
+    variables           = map(string)
+    vpc_config          = map(any)
+  }))
 }
